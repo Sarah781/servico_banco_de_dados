a923b00cb89b48f0b95e9fd7e6a2c8361a72b330
Sarah Santana <72801105+Sarah781@users.noreply.github.com>
2022-11-04 07:52:55 -0300

    Update model.ts

--- src/models/model.ts
+++ src/models/model.ts
@@ -1,3 +1,4 @@
+import internal = require("stream")
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
 @Entity()
@@ -17,6 +18,15 @@ export class Projeto {
 
     @Column()
     anoFim: number
+
+    constructor(id: number, nome: string, linguagem: string, anoInicio: number, anoFim: number) {
+        this.id = id
+        this.nome = nome
+        this.linguagem = linguagem
+        this.anoInicio = anoInicio
+        this.anoFim = anoFim
+    }
+
 }
 
 @Entity()
