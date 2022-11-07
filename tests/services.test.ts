8255a7355242f52cddea2de6d679d91f7832f704
Sarah Santana <72801105+Sarah781@users.noreply.github.com>
2022-11-07 20:22:56 -0300

    Update services.test.ts

--- tests/services.test.ts
+++ tests/services.test.ts
@@ -4,7 +4,7 @@ it("validateNome with a invalid nome", () => {
     const validator = new Validator();
     const error = validator.validateNome("A1");
     expect(error).not.toBeNull();
-    expect(error.message).toBe("123");
+    expect(error.message).toBe("Nome deve ter pelo menos 3 caracteres");
 });
 
 it("validateNome with a valid nome", () => {
